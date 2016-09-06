c5b340e3f527edb503db1f459cd12ed1bfd24d30
Ana Cruz <anacruz1818@gmail.com>
2016-09-06 10:49:11 -0500

    change 1. Add print message

--- PruebaGit/ViewController.swift
+++ PruebaGit/ViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("change 1")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
